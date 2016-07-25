34521bc2b849e65f8ba7bf504f231973cc061371
Nazish Ali <nazish.ali@quickli.com>
2016-07-25 13:43:34 +0530

    add header file

--- Example/UIBasedSdk/HelpDeskViewController.h
+++ Example/UIBasedSdk/HelpDeskViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-//#import <UIBasedSdk/>
+
 
 @interface HelpDeskViewController : UIViewController
 
---
+++ UIBasedSdk/Classes/Header.h
@@ -0,0 +1,9 @@
+//
+//  Header.h
+//  UIBasedSdk
+//
+//  Created by Quickli on 25/07/16.
+//  Copyright © 2016 Nazish Ali. All rights reserved.
+//
+
+#import "DeviceLibrary.h"
